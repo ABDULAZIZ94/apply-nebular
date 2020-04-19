1a16c952c6af9ab5877b573cd80a00d9a926674a
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2020-04-19 21:45:42 +0800

    added nb-layout, nb-sidebar, nb-card,  and nbInput

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy,Component } from '@angular/core';
+import { NbSidebarService } from '@nebular/theme';
+
 
 @Component({
   selector: 'app-root',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'apply-nebular';
+
+  constructor( private sidebarService: NbSidebarService){
+
+  }
+
+  toggle() {
+    this.sidebarService.toggle();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NbThemeModule, NbLayoutModule, NbInputModule } from '@nebular/theme';
+import { NbSidebarModule, NbCardModule } from '@nebular/theme';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbEvaIconsModule } from '@nebular/eva-icons';//add nebular theme
+import { NbIconModule } from '@nebular/theme';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbIconModule,
+    NbCardModule,
+    NbInputModule,
+    NbSidebarModule.forRoot(),
+    NbThemeModule.forRoot({ name: 'default' }),
   ],
   providers: [],
   bootstrap: [AppComponent]
